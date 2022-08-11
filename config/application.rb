27578797d79d015cd5c8b19312707b5ec6330328
Ideilson Cisne <deilson.cisne@gmail.com>
2022-08-11 12:03:27 -0300

    Adicionando modulo de segurança rack-attack

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module OneBitContacts
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*' # Qualquer origins de site será chamado.
@@ -39,5 +40,7 @@ module OneBitContacts
           methods: %i(get post put patch delete options head)
       end
     end
+
+    config.middleware.use Rack::Attack
   end
 end
